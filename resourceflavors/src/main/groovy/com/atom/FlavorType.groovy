06956785a7b2796dd1f4bc8ee6ff0835a42c03b1
atom <qiuyunfei@rsung.com>
2018-06-01 15:08:49 +0800

    非打包时优化

--- resourceflavors/src/main/groovy/com/atom/FlavorType.groovy
+++ resourceflavors/src/main/groovy/com/atom/FlavorType.groovy
@@ -1,7 +1,12 @@
 package com.atom
 
 public class FlavorType {
-
+    /**
+     * 存放渠道包图片的路径
+     */
     String resourceDir
+    /**
+     * 主项目名
+     */
     String appName
 }
--- resourceflavors/src/main/groovy/com/atom/ResourceFlavorsPlugin.groovy
+++ resourceflavors/src/main/groovy/com/atom/ResourceFlavorsPlugin.groovy
@@ -14,12 +14,21 @@ public class ResourceFlavorsPlugin implements Plugin<Project> {
             FlavorType ext = project.rfp
             def resourceDir = ext.resourceDir
             def appName = ext.appName
+            //非打包时不做任何操作
+            def startParameter = project.gradle.startParameter
+            def targetTasks = startParameter.taskNames
+
+            targetTasks.each {
+                if (!it.contains("assemble") && !it.contains("aR")) {
+                    return
+                }
+            }
 
             project.android.applicationVariants.all { variant ->
                 String variantName = variant.name.capitalize()
                 def variantFlavorName = variant.flavorName
                 Task preBuild = project.tasks["pre${variantName}Build"]
-                if (variantFlavorName==null||"" == variantFlavorName){
+                if (variantFlavorName == null || "" == variantFlavorName) {
                     return
                 }
                 preBuild.doFirst {
